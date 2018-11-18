9d90a2019c41a0f91c1c11956a4162b9fd5e8626
earlyano07 <yudaearlyano@gmail.com>
2018-11-19 00:23:19 +0700

    Retrofit_Earlyano

---
+++ app/src/main/java/com/example/earlyano/modul10_retrofit/Adapter/ClickListener.java
@@ -0,0 +1,10 @@
+package com.example.earlyano.modul10_retrofit.Adapter;
+
+import android.view.View;
+
+public interface ClickListener {
+
+    public void onClick(View view, int position);
+    public void onLongClick(View view,int position);
+
+}
---
+++ app/src/main/java/com/example/earlyano/modul10_retrofit/Rest/ApiClient.java
@@ -0,0 +1,25 @@
+package com.example.earlyano.modul10_retrofit.Rest;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+// Untuk mengirim permintaan ke API, kita perlu menggunakan class builder retrofit
+// dan menentukan URL dasar untuk layanan tersebut
+
+public class ApiClient {
+    //Untuk mengirim permintaan jaringan ke API,
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+
+        if(retrofit==null){
+            //Untuk mengirim permintaan jaringan ke API,
+            // kita perlu menggunakan kelas builder Retrofit dan menentukan URL dasar untuk layanan tersebut.
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return  retrofit;
+    }
+
+}
+
